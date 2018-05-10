0df52070d37803810901d12a6ee07bb22f7e3abd
davesag <davesag@gmail.com>
2018-05-10 08:29:07 +0530

    n/a updated dependencies to fix node 10 incompatibility

--- test/unit/test_helper.js
+++ test/unit/test_helper.js
@@ -2,8 +2,6 @@ const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const chaiAsPromised = require('chai-as-promised')
-const sinonStubPromise = require('sinon-stub-promise')
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
-sinonStubPromise(sinon)
